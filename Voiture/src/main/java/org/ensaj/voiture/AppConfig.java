641cacce1995d684a7c347465aebee526ac15a50
reda <redamakaoui2001@gmail.com>
2023-12-07 15:46:46 +0100

    deploiment docker

---
+++ Voiture/src/main/java/org/ensaj/voiture/AppConfig.java
@@ -0,0 +1,23 @@
+package org.ensaj.voiture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:application-docker.properties")
+public class AppConfig {
+    private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+    private final Environment environment;
+
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+        logger.info("AppConfig initialized with profile: {}", String.join(",", environment.getActiveProfiles()));
+    }
+
+    private String getActiveProfile() {
+        return String.join(",", environment.getActiveProfiles());
+    }
+}
